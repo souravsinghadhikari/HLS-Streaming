b72cee53e02de6b3a09ac7358225cbaaf96113d4
Sourav Singh Adhikari <souravadhikari668@gmail.com>
2025-05-27 02:01:50 +0530

    Ground Work Done

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('HLS streaming server running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
